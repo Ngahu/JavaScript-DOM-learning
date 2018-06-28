8d915581a915b6f2b291fec935c8cc6f4d7250be
Ngahu <jamaalaraheem@gmail.com>
2018-06-28 14:03:47 +0300

    accessing the dom elements using js

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log(10)
+// EXAMINE THE DOCUMENT OBJECT //
+console.dir(document);
+
+// get the domain
+console.log(document.domain);
+
+
+// get the full url
+console.log(document.URL)
+
+
+// get the title
+console.log(document.title);
+
+
+console.log(document.all)
